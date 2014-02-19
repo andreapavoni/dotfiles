1f35c47f9549f1a394e274bfeafbde878af796b4
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-02-19 17:56:12 +0100

    fix zsh git prompt for non tracked files

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,13 +5,14 @@ zstyle ':vcs_info:*' stagedstr '!'
 zstyle ':vcs_info:*' unstagedstr '?'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats '%F{5}[%F{2}%b%F{5}%F{1}%c%F{3}%u%F{5}]%f'
+zstyle ':vcs_info:*' formats '%F{5}[%F{2}%b%F{5} %F{3}%u%F{1}%c%F{5}]%f'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 zstyle ':vcs_info:*' enable git
 +vi-git-untracked() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
     git status --porcelain | grep '??' &> /dev/null ; then
-    hook_com[unstaged]+='%F{1}??%f'
+    # hook_com[unstaged]+='%F{9}*%f'
+    hook_com[unstaged]="%F{9}*%F{3}${hook_com[unstaged]}"
   fi
 }
 

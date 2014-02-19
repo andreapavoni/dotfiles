be32d5d0b995d51d7d68c2129b071dfadf6e2406
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-02-19 17:26:33 +0100

    fix zsh git and colors prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,26 +1,20 @@
-function git_prompt_info {
-  local ref=$(=git symbolic-ref HEAD 2> /dev/null)
-  local gitst="$(=git status 2> /dev/null)"
+setopt prompt_subst
+autoload -Uz vcs_info
 
-  if [[ -f .git/MERGE_HEAD ]]; then
-    if [[ ${gitst} =~ "unmerged" ]]; then
-      gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
-    else
-      gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
-    fi
-  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-    gitstatus=" %{$fg[red]%}?%{$reset_color%}"
-  elif [[ ${gitst} =~ "use \"git add" ]]; then
-    gitstatus=" %{$fg[red]%}!%{$reset_color%}"
-  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-    gitstatus=" %{$fg[yellow]%}*%{$reset_color%}"
-  else
-    gitstatus=''
-  fi
-
-  if [[ -n $ref ]]; then
-    echo "%{$fg[magenta]%}[%{$fg[green]%}${ref#refs/heads/}%{$fg[magenta]%}$gitstatus]%{$reset_color%}"
+zstyle ':vcs_info:*' stagedstr '!'
+zstyle ':vcs_info:*' unstagedstr '?'
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+zstyle ':vcs_info:*' formats '%F{5}[%F{2}%b%F{5}%F{1}%c%F{3}%u%F{5}]%f'
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+zstyle ':vcs_info:*' enable git
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+    git status --porcelain | grep '??' &> /dev/null ; then
+    hook_com[unstaged]+='%F{1}??%f'
   fi
 }
 
-PROMPT="%{$fg[green]%}%n@%m%b %{$fg[yellow]%}%~%<< $(git_prompt_info)${PR_BOLD_WHITE}%#%{${reset_color}%} "
+precmd () { vcs_info }
+
+PROMPT='%F{5}%F{2}%n@%m%b%F{5} %F{3}%3~ ${vcs_info_msg_0_} %f%# '

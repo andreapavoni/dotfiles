188931e05098ca02ffb3061f65a7847ce3b58bad
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-02-20 09:55:19 +0100

    use emacs bindings instead of vim for zsh

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -6,10 +6,10 @@ bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "^[[A" history-beginning-search-backward       # up arrow
-bindkey "^[[B" history-beginning-search-forward        # down arrow
+bindkey "^[[A"    history-beginning-search-backward    # up arrow
+bindkey "^[[B"    history-beginning-search-forward     # down arrow
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
-bindkey -v   # Default to standard vi bindings, regardless of editor string
+bindkey -e   # Default to standard vi bindings, regardless of editor string
 

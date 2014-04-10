2d864b148548f86d3c9fbf63781bbcf93a22732d
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-04-11 00:14:29 +0200

    add more settings for ruby 2.1.x

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,5 +1,6 @@
-alias b="bundle"
-alias be="bundle exec"
-alias ber="bundle exec rspec"
-alias r="rails"
+alias b='bundle'
+alias be='bundle exec'
+alias ber='bundle exec rspec'
+alias r='rails'
+alias s='spring'
 # alias rdb='rake db:migrate db:rollback && rake db:migrate db:test:prepare'

f322b8b314fe0505f32b3f979aefe12a292ccc63
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-07-09 14:40:46 +0200

    Fix ruby heap env

--- ruby/envs.zsh
+++ ruby/envs.zsh
@@ -1,10 +1,6 @@
-if [[ $(ruby -v | awk '{print $2}' | cut -d '.' -f '1,2') == '2.1' ]]; then
-  export RUBY_GC_HEAP_INIT_SLOTS=1000000 # ruby >= 2.1.x
-else
-  export RUBY_HEAP_MIN_SLOTS=1000000 # ruby < 2.1.x
-fi
-
 export RUBY_HEAP_SLOTS_INCREMENT=1000000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=100000000
 export RUBY_HEAP_FREE_MIN=500000
+export RUBY_HEAP_MIN_SLOTS=1000000 # ruby < 2.1.x
+export RUBY_GC_HEAP_INIT_SLOTS=1000000 # ruby >= 2.1.x

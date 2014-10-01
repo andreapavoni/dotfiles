d23d79d44f44914f690d7c9d1e253e8105f3d527
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-10-01 11:01:20 +0200

    Fix autocompletion errors from Ag

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -29,10 +29,10 @@ unsetopt beep
 # setopt share_history # imports new commands and appends typed commands to history
 
 # # ===== Completion
-# setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
-# setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
-# setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
-# setopt complete_in_word # Allow completion from within a word/phrase
+setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
+setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
+setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
+setopt complete_in_word # Allow completion from within a word/phrase
 
 # unsetopt menu_complete # do not autoselect the first completion entry
 

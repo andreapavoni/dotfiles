2e55f7a2d4cfb8de00cbbf322235c1f79562b638
Andrea Pavoni <andrea.pavoni@gmail.com>
2014-12-17 18:20:42 +0100

    setup base16 256 colors on vim and shell

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -20,3 +20,7 @@ unset LSCOLORS
 
 export CLICOLOR=1
 export LS_COLORS=exfxcxdxbxegedabagacad
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.dotfiles/zsh/base16-tomorrow.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL

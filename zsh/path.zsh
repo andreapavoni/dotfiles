75bbf99c4edeb6cec9fcc98a8d9e9da1da3fc793
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-05-15 11:26:45 +0200

    update configs, add vim syntaxes

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin/stubs:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin/stubs:/usr/local/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 

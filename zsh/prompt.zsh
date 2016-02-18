a4ef4fcd7cc8e343e16a24e1afae8f66ca60130b
Andrea Pavoni <andrea.pavoni@gmail.com>
2016-02-18 11:52:38 +0100

    update zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -18,4 +18,4 @@ zstyle ':vcs_info:*' enable git
 precmd () { vcs_info }
 
 # PROMPT='%F{5}%F{2}%n@%m%b%F{5} %F{3}%3~ ${vcs_info_msg_0_} %f%# '
-PROMPT='%F{5}%F{2}%m:%b%F{5}%F{5}[%F{3}%3~%F{5}]${vcs_info_msg_0_}%F{1}λ%f '
+PROMPT='%F{5}%F{2}%m:%b%F{5}%F{5}[%F{3}%3~%F{5}]${vcs_info_msg_0_}%F{1} λ%f '

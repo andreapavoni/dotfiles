c0e63f46b5a1fc36ec3b80e42db6daa4711519ce
Andrea Pavoni <andrea.pavoni@prima.it>
2020-12-29 16:37:41 +0100

    remove atom editor package list

--- go/envs.zsh
+++ go/envs.zsh
@@ -1,4 +1,5 @@
 # GoLang
+export GOROOT="$(asdf where golang)/go/"
 export GOPATH=$HOME/Code/go-code
 export PATH=$PATH:$GOPATH/bin # add Go binaries in default $PATH
 alias='gofmt -s -tabs=false -tabwidth=2'

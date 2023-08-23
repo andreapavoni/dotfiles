037a9c6ac40a8b5980233495796cfad76f6776c0
Andrea Pavoni <andrea.pavoni@treatwell.com>
2023-08-23 15:44:15 +0200

    neovim: fix auto-session restore

--- vim/nvim/lua/user/session.lua
+++ vim/nvim/lua/user/session.lua
@@ -5,15 +5,23 @@ end
 
 auto_session.setup(
   {
-    -- log_level = 'info',
-    auto_session_enable_last_session = true,
+    log_level = 'info',
+    auto_session_enable_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
     auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
     auto_session_enabled = true,
     auto_save_enabled = true,
-    auto_restore_enabled = true,
+    -- auto_restore_enabled = true,
     -- auto_session_suppress_dirs = nil,
     -- auto_session_use_git_branch = nil,
     -- the configs below are lua only
     -- bypass_session_save_file_types = nil
+
+    -- cwd_change_handling = {
+    --   restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+    --   pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+    --   post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+    --   require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+    -- end,
+    -- },
   }
 )
